e3d4263925f2361d789e99f015e3ce69fe95c8ad
taehoon0518 <cjsxogns0518@gmail.com>
2025-02-12 21:03:46 +0900

    [Feat] 페이지네이션 이욯한 여러 회원 조회 기능 추가

--- src/main/java/com/example/idus_exam/member/model/MemberDto.java
+++ src/main/java/com/example/idus_exam/member/model/MemberDto.java
@@ -2,6 +2,7 @@ package com.example.idus_exam.member.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.domain.Page;
 
 public class MemberDto {
 
@@ -42,6 +43,24 @@ public class MemberDto {
                     .gender(member.getGender())
                     .build();
 
+        }
+    }
+    @Getter
+    @Builder
+    public static class listResponse {
+        String name;
+        String nickname;
+        String phoneNum;
+        String email;
+        String gender;
+        public static listResponse from (Member member) {
+            return listResponse.builder()
+                    .name(member.getName())
+                    .nickname(member.getNickname())
+                    .phoneNum(member.getPhoneNum())
+                    .email(member.getEmail())
+                    .gender(member.getGender())
+                    .build();
         }
     }
 }
